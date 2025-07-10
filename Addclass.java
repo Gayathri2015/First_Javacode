a9cf474e3ebf44f68faa96a7be7131097b6b458e
gayathri <gayathrimakineni1301@gmail.com>
2025-07-09 22:01:58 -0500

    Adding two integers

---
+++ Addclass.java
@@ -0,0 +1,14 @@
+public class Addclass {
+    
+    public void add(int a, int b){
+        int c;
+        c = a+b;
+        System.out.println("The sum is" + c);
+    }
+    public static void main(String[] args){
+        Addclass a = new Addclass();
+        a.add(5,6);
+
+
+    }
+}
