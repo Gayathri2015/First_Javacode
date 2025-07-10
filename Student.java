506cb0441a9fcc44dd0f6fa4ddc5526c97cbfae4
gayathri <gayathrimakineni1301@gmail.com>
2025-07-10 16:05:59 -0500

    Object program commit

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student {
+
+    String name;
+    int rollno;
+
+    Student(String name,int rollno){
+        this.name = name;
+        this.rollno = rollno;
+    }
+
+    public void display(){
+        System.out.println("The name of the student is " + name + " the rollno is " + rollno);
+    }
+
+    public static void main(String[] args){
+
+    Student s1 = new Student("Jagan", 101);
+
+    s1.display();
+
+    }
+    
+}
